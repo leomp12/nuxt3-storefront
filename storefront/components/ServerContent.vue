b49e78465db0dfd31fde05b1b390805520b3a573
Leonardo Matos <leomp120894@gmail.com>
2022-02-21 14:50:54 -0300

    Testing server only async dynamic content load

---
+++ storefront/components/ServerContent.vue
@@ -0,0 +1,15 @@
+<script setup>
+  let description = 'Description ...';
+  if (process.server) {
+    description = await new Promise((resolve) => {
+      console.log('Loading description...');
+      setTimeout(() => {
+        resolve('<h1>Hello Nuxt 3</h1><h3>Storefront PoC With Vue 3</h3>');
+      }, 200);
+    });
+  }
+</script>
+
+<template>
+  <div v-html="description"></div>
+</template>
---
+++ storefront/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <ServerContent/>
+    <h4>Client side content here</h4>
+  </div>
+</template>

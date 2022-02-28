350a8b4504c5e57264d3fcf043769549f14d2b2b
Leonardo Matos <leomp120894@gmail.com>
2022-02-28 15:35:54 -0300

    Remove global set component and make index SSRed with rand

--- storefront/pages/index.vue
+++ storefront/pages/index.vue
@@ -1,4 +1,9 @@
 <template>
 <h1>Welcome to Nuxt 3</h1>
 <p>Visit <a href="https://v3.nuxtjs.org/">https://v3.nuxtjs.org/</a> to read the documentation</p>
+<p>rand: {{ rand }}</p>
 </template>
+
+<script setup>
+let rand = parseInt(Math.random() * 1000, 10);
+</script>
--- storefront/pages/product.vue
+++ storefront/pages/product.vue
@@ -1,7 +1,22 @@
 <template>
 <div>
-  <ServerContent/>
+  <div v-html="description"></div>
   <hr>
   <h4>Client side content here</h4>
 </div>
 </template>
+
+<script setup>
+import ecomUtils from '@ecomplus/utils/dist/ecom-utils.node.min';
+import ecomClient from '@ecomplus/client/dist/ecom-client.node.js';
+
+let description = 'Mocked description ...';
+if (process.server) {
+  console.log('Loading product description ...');
+  ecomUtils.$ecomConfig.set('store_id', 1024);
+  const { data } = await ecomClient.store({
+    url: '/products/5d09197e24607a6a42d7140d',
+  });
+  description = data.body_html;
+}
+</script>

6657808419320c5ffd1bbb8e1d55dc4b0f66e1dd
Leonardo Matos <leomp120894@gmail.com>
2022-04-20 19:43:35 -0300

    Updating to Nuxt 3 RC

--- storefront/pages/index.vue
+++ storefront/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-<h1>Welcome to Nuxt 3</h1>
+<h1>Welcome to Nuxt 3 RC</h1>
 <p>Visit <a href="https://v3.nuxtjs.org/">https://v3.nuxtjs.org/</a> to read the documentation</p>
 <p>rand: {{ rand }}</p>
 </template>

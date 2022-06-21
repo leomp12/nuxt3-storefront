d15af041085992cc32db3c1e655e3a5237fa1098
Leonardo Matos <leomp120894@gmail.com>
2022-06-21 17:56:25 -0300

    feat: Loading products description from Store API v2

--- storefront/pages/product.vue
+++ storefront/pages/product.vue
@@ -2,20 +2,18 @@
 <div>
   <div v-html="description"></div>
   <hr>
-  <h4>Client side content here</h4>
+  <h4>Client side content here !!</h4>
 </div>
 </template>
 
 <script setup>
-import ecomUtils from '@ecomplus/utils/dist/ecom-utils.node.min';
-import ecomClient from '@ecomplus/client/dist/ecom-client.node.js';
+import api from '@cloudcommerce/api';
 
 let description = 'Mocked description ...';
 if (process.server) {
   console.log('Loading product description ...');
-  ecomUtils.$ecomConfig.set('store_id', 1024);
-  const { data } = await ecomClient.store({
-    url: '/products/5d09197e24607a6a42d7140d',
+  const { data } = await api.get('products/5c701c8ac626be23430d4fe5', {
+    storeId: 1011,
   });
   description = data.body_html;
 }
